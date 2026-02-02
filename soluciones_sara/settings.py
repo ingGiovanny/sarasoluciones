4a85283ef94f5758e40b657ce31ffbbd17b605b8
ing-Gio <geigpea@soy.sena.edu.co>
2026-02-02 06:38:46 -0500

    BASE DE DATOS MYSQL

--- soluciones_sara/settings.py
+++ soluciones_sara/settings.py
@@ -88,10 +88,15 @@ WSGI_APPLICATION = 'soluciones_sara.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_sena',
+        'USER': 'root',
+        'PASSWORD': 'Camilo120',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
+
 
 
 # Password validation
