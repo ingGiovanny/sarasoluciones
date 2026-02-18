b10905d84204ed4343731a3ab698f7efac1e3e7a
SofiaPirasan <laura.pirasan@soy.sena.edu.co>
2026-02-18 12:22:30 -0500

    registro cambios

--- mi_app/view/detalle_producto/detalle_producto_views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render, get_object_or_404
-from mi_app.models import Producto
-
-def detalle_producto(request, pk):
-    # Buscamos el producto por su llave primaria (pk)
-    producto = get_object_or_404(Producto.objects.select_related('id_presentacion', 'id_marca'), pk=pk)
-    return render(request, 'principalclientes/listar/detalle_producto.html', {'producto': producto})
--- mi_app/view/principalcliente/views_principal_cliente.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-def pagina_clientes(request):
-    return render(request, 'principalclientes/contenido.html')
-
-
-
