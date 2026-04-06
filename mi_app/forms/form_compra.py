47d09d8a5c8945ecdbdcbee155f788def2c329b8
ing-Gio <giovannygps05@gmail.com>
2026-04-06 16:05:52 -0500

    formulario de compra para calendario

--- mi_app/forms/form_compra.py
+++ mi_app/forms/form_compra.py
@@ -1,49 +1,32 @@
 from django import forms
-from django.forms import ModelForm, TextInput, EmailInput, NumberInput, PasswordInput, Select, DateInput
+from django.forms import ModelForm, TextInput, NumberInput, Select, DateInput
 from mi_app.models import Compra
 
 class CompraForm(ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['id_administrador'].widget.attrs.update({'class': 'form-control'})
+        # Aplicamos la clase de Bootstrap a todos los campos de forma automática
+        for field in self.fields.values():
+            field.widget.attrs.update({'class': 'form-control'})
 
     class Meta:
         model = Compra
         fields = '__all__'
         widgets = {
-            'id_administrador': Select(
-                attrs={
-                    'placeholder': 'Ingrese el id del administrador',
-                }
-            ),
-            'id_proveedor': Select(
-                attrs={
-                    'placeholder': 'Ingrese el id del proveedor',
-                }
-            ),
-            'id_producto': Select(
-                attrs={
-                    'placeholder': 'Ingrese el id del producto',
-                }
-            ),
-            'cantidad_productos': NumberInput(
-                attrs={
-                    'placeholder': 'Ingrese la cantidad de productos',
-                }
-            ),
-            'observaciones': TextInput(
-                attrs={
-                    'placeholder': 'Ingrese las observaciones',
-                }
-            ),
+            'id_administrador': Select(attrs={'placeholder': 'Seleccione administrador'}),
+            'id_proveedor': Select(attrs={'placeholder': 'Seleccione proveedor'}),
+            'id_producto': Select(attrs={'placeholder': 'Seleccione producto'}),
+            'cantidad_productos': NumberInput(attrs={'placeholder': 'Cantidad'}),
+            'observaciones': TextInput(attrs={'placeholder': 'Observaciones adicionales'}),
+            
+            # 🔥 AQUÍ ESTÁ EL CAMBIO: type='date' activa el calendario nativo
             'fecha_compra': DateInput(
                 attrs={
-                    'placeholder': 'Ingrese la fecha de la compra',
-                }
-            ),   
-            'valor_total': NumberInput(
-                attrs={
-                    'placeholder': 'Ingrese el valor total',
-                }
-            ),
+                    'type': 'date', 
+                    'class': 'form-control'
+                }, 
+                format='%Y-%m-%d'
+            ), 
+            
+            'valor_total': NumberInput(attrs={'placeholder': 'Total de la compra'}),
         }
